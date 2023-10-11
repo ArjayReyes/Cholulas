8211b3d20476628ffed08f458ec5bf0417c8dc3d
Arjay <142987195+ArjayReyes@users.noreply.github.com>
2023-10-10 19:49:58 -0700

    Update database.java

--- database.java
+++ database.java
@@ -1,14 +1,36 @@
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Database {
     private static HashMap<String, User> users = new HashMap<>();
+    private static HashMap<String, List<NewsPost>> newsPosts = new HashMap<>();
 
     public static void addUser(String username, String password) {
         User user = new User(username, password);
         users.put(username, user);
+        newsPosts.put(username, new ArrayList<>());
     }
 
     public static User getUser(String username) {
         return users.get(username);
+    }
+
+    public static List<NewsPost> getNewsPosts(String username) {
+        return newsPosts.get(username);
+    }
+
+    public static void addNewsPost(String username, NewsPost newsPost) {
+        List<NewsPost> userPosts = newsPosts.get(username);
+        if (userPosts != null) {
+            userPosts.add(newsPost);
+        }
+    }
+
+    public static void deleteNewsPost(String username, int index) {
+        List<NewsPost> userPosts = newsPosts.get(username);
+        if (userPosts != null && index >= 0 && index < userPosts.size()) {
+            userPosts.remove(index);
+        }
     }
 }
