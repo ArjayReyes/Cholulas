2a4e3ea4488e92545c7132e1aef71f42a9e58cf2
Arjay <142987195+ArjayReyes@users.noreply.github.com>
2023-10-10 19:50:16 -0700

    Create NewsPost.java

---
+++ NewsPost.java
@@ -0,0 +1,17 @@
+public class NewsPost {
+    private String title;
+    private String content;
+
+    public NewsPost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
